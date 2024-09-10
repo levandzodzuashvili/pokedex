b38a43c6cab23270ffbd3dd83d76350325c3bfea
Levan Dzodzuashvili <levandzodzuashvili@mziuri.ge>
2024-09-10 20:51:00 +0400

    .

--- app.js
+++ app.js
@@ -1,58 +1,64 @@
-const colors = {
-  fire: "#FDDFDF",
-  grass: "#DEFDE0",
-  electric: "#FCF7DE",
-  water: "#DEF3FD",
-  ground: "#f4e7da",
-  rock: "#d5d5d4",
-  fairy: "#fceaff",
-  poison: "#98d7a5",
-  bug: "#f8d5a3",
-  dragon: "#97b3e6",
-  psychic: "#eaeda1",
-  flying: "#F5F5F5",
-  fighting: "#E6E0D4",
-  normal: "#F5F5F5",
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const colors = {
+    fire: "#FDDFDF",
+    grass: "#DEFDE0",
+    electric: "#FCF7DE",
+    water: "#DEF3FD",
+    ground: "#f4e7da",
+    rock: "#d5d5d4",
+    fairy: "#fceaff",
+    poison: "#98d7a5",
+    bug: "#f8d5a3",
+    dragon: "#97b3e6",
+    psychic: "#eaeda1",
+    flying: "#F5F5F5",
+    fighting: "#E6E0D4",
+    normal: "#F5F5F5",
+  };
 
-const pokedex = document.getElementById("pokedex");
-const loadMoreButton = document.getElementById("load-more");
+  const pokedex = document.getElementById("pokedex");
+  const loadMoreButton = document.getElementById("load-more");
 
-let allPokemon = []; // Array to store all loaded Pokémon ამას გამოიყენებ რო შეინახო ერეიში ყველა ელემენტი
-let lastPokemonId = 50;
-const loadMorePokemons = 10;
-
-const getPokemon = () => {
-  let promises = [];
-  for (let i = 1; i <= 50; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
+  if (!pokedex) {
+    console.error("POKEDEX ELEMENT NOT FOUND");
+    return;
   }
-  Promise.all(promises).then((result) => {
-    const pokemon = result.map((data) => ({
-      id: data.id,
-      name: data.name,
-      image: data.sprites["front_default"],
-      types: data.types.map((typeInfo) => typeInfo.type.name),
-    }));
-    allPokemon = allPokemon.concat(pokemon); // Add new Pokémon to allPokemon array კონკატი გინდა იმისთვის რო გააერთიანოს ახალი დატა რაც მიიღებს პრომისესიდან
-    displayPokemon(allPokemon); // ამას გამოიყენებ რო დაარენდერო ყველა პოკემონი
-  });
-};
 
-const displayPokemon = (pokemon) => {
-  const pokemonString = pokemon
-    .map((singlePokemon) => {
-      const typePills = singlePokemon.types
-        .map(
-          (type) =>
-            `<a href="#" class="pill" style="background-color:${
-              colors[type] || "#FFFFFF"
-            }">${type}</a>`
-        )
-        .join("");
+  let allPokemon = []; // Array to store all loaded Pokémon ამას გამოიყენებ რო შეინახო ერეიში ყველა ელემენტი
+  let lastPokemonId = 50;
+  const loadMorePokemons = 10;
 
-      return `
+  const getPokemon = () => {
+    let promises = [];
+    for (let i = 1; i <= 50; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((result) => {
+      const pokemon = result.map((data) => ({
+        id: data.id,
+        name: data.name,
+        image: data.sprites["front_default"],
+        types: data.types.map((typeInfo) => typeInfo.type.name),
+      }));
+      allPokemon = allPokemon.concat(pokemon); // Add new Pokémon to allPokemon array კონკატი გინდა იმისთვის რო გააერთიანოს ახალი დატა რაც მიიღებს პრომისესიდან
+      displayPokemon(allPokemon); // ამას გამოიყენებ რო დაარენდერო ყველა პოკემონი
+    });
+  };
+
+  const displayPokemon = (pokemon) => {
+    const pokemonString = pokemon
+      .map((singlePokemon) => {
+        const typePills = singlePokemon.types
+          .map(
+            (type) =>
+              `<a href="#" class="pill" style="background-color:${
+                colors[type] || "#FFFFFF"
+              }">${type}</a>`
+          )
+          .join("");
+
+        return `
         <div class="pokemon-icon" data-id="${singlePokemon.id}" onclick="location.href='pokeinfo.html?id=${singlePokemon.id}'">
           <img class="pic" src="${singlePokemon.image}" />
           <div class="info">
@@ -61,61 +67,68 @@ const displayPokemon = (pokemon) => {
           </div>
           <div class="type-container">${typePills}</div>
         </div>`;
-    })
-    .join("");
-  pokedex.innerHTML = pokemonString;
-};
+      })
+      .join("");
+    pokedex.innerHTML = pokemonString;
+  };
+  if (loadMoreButton != null) {
+    loadMoreButton.addEventListener("click", function () {
+      let promises = [];
+      for (
+        let i = lastPokemonId + 1;
+        i <= lastPokemonId + loadMorePokemons;
+        i++
+      ) {
+        promises.push(
+          fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) =>
+            res.json()
+          )
+        );
+      }
 
-loadMoreButton.addEventListener("click", function () {
-  let promises = [];
-  for (let i = lastPokemonId + 1; i <= lastPokemonId + loadMorePokemons; i++) {
-    promises.push(
-      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) => res.json())
-    );
+      Promise.all(promises).then((result) => {
+        const pokemon = result.map((data) => ({
+          id: data.id,
+          name: data.name,
+          image: data.sprites.front_default,
+          types: data.types.map((typeInfo) => typeInfo.type.name),
+        }));
+        allPokemon = allPokemon.concat(pokemon); //  აქედან დაამატებ ახალ პოკემონებს ყველა პოკემონის მასივში
+        displayPokemon(allPokemon); // ამას გამოიყენებ რო დაარენდერო ყველა პოკემონი
+        lastPokemonId += loadMorePokemons;
+      });
+    });
   }
 
-  Promise.all(promises).then((result) => {
-    const pokemon = result.map((data) => ({
-      id: data.id,
-      name: data.name,
-      image: data.sprites.front_default,
-      types: data.types.map((typeInfo) => typeInfo.type.name),
-    }));
-    allPokemon = allPokemon.concat(pokemon); //  აქედან დაამატებ ახალ პოკემონებს ყველა პოკემონის მასივში
-    displayPokemon(allPokemon); // ამას გამოიყენებ რო დაარენდერო ყველა პოკემონი
-    lastPokemonId += loadMorePokemons;
-  });
-});
+  getPokemon();
 
-getPokemon();
+  document.querySelectorAll(".pokemon-icon").forEach((icon) => {
+    icon.addEventListener("click", async function () {
+      const pokemonId = this.getAttribute("data-id");
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+      );
+      const pokeInfo = await response.json();
 
-document.querySelectorAll(".pokemon-icon").forEach((icon) => {
-  icon.addEventListener("click", async function () {
-    const pokemonId = this.getAttribute("data-id");
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
-    );
-    const pokeInfo = await response.json();
+      const pokeKey = pokeInfo.id;
+      const pokeImg = pokeInfo.data.sprites.front_default;
+      const pokeName = pokeInfo.name;
+      const pokeAbilities = pokeInfo.abilities;
+      const pokeBaseExp = pokeInfo.base_experience
+        .map((ability) => ability.ability.name)
+        .join(", ");
+      const pokeHight = pokeInfo.height;
+      const pokeWeight = pokeInfo.weight;
+      const pokeForms = pokeInfo.forms.map((form) => form.name).join(", ");
+      const pokeItems = pokeInfo.held_items
+        .map((item) => item.item.name)
+        .join(", ");
+      const pokeStats = pokeInfo.stats
+        .map((stat) => `${stat.stat.name}: ${stat.base_stat}`)
+        .join(", ");
+      const pokeTypes = pokeInfo.types.map((type) => type.type.name).join(", ");
 
-    const pokeKey = pokeInfo.id;
-    const pokeImg = pokeInfo.data.sprites.front_default;
-    const pokeName = pokeInfo.name;
-    const pokeAbilities = pokeInfo.abilities;
-    const pokeBaseExp = pokeInfo.base_experience
-      .map((ability) => ability.ability.name)
-      .join(", ");
-    const pokeHight = pokeInfo.height;
-    const pokeWeight = pokeInfo.weight;
-    const pokeForms = pokeInfo.forms.map((form) => form.name).join(", ");
-    const pokeItems = pokeInfo.held_items
-      .map((item) => item.item.name)
-      .join(", ");
-    const pokeStats = pokeInfo.stats
-      .map((stat) => `${stat.stat.name}: ${stat.base_stat}`)
-      .join(", ");
-    const pokeTypes = pokeInfo.types.map((type) => type.type.name).join(", ");
-
-    const pokeValue = `
+      const pokeValue = `
       <div class="about-poke" href="pokeinfo.html">
         <img class="image" src="${pokeImg}" />
         <div class="nomeri">ID: ${pokeKey}</div>
@@ -131,8 +144,8 @@ document.querySelectorAll(".pokemon-icon").forEach((icon) => {
       </div>
     `;
 
-    localStorage.setItem(pokeKey, pokeValue);
-    window.location.href = `pokeinfo.html?id=${pokeKey}`;
-    displayPokemon(pokeValue);
+      localStorage.setItem(pokeKey, pokeValue);
+      window.location.href = `pokeinfo.html?id=${pokeKey}`;
+    });
   });
 });
